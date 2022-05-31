2b0d7a4c8fae33ef238503e43cde7ab2e75276bd
Pushkal Kishore <kpushkal122@gmail.com>
2022-05-31 13:06:52 +0530

    Initial project setup

---
+++ src/components/IntroPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+import bg from "../images/bg1.jpg";
+import "./IntroPage.css"
+import Typewriter from "typewriter-effect"
+
+export default function IntroPage(){
+    return (
+        <Parallax bgImage={bg} strength={300}>
+        <div className="intropage">
+            <h1>Pushkal Kishore</h1>
+            <div className="typewriter">
+            <Typewriter
+                options={{
+                    strings: ["Programmer", "Developer", "Cool Guy" , "Learner", "Thinker"],
+                    autoStart: true,
+                    loop: true,
+                }}
+			/>
+            </div>
+        </div>
+        </Parallax>
+    )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+import bg from "../images/bg1.jpg";
+import "./Skills.css"
+
+export default function Skills(){
+    return (
+        
+        <div className="skills">
+            <h1>Skills</h1>
+            <h2>C++</h2>
+            <h2>Python</h2>
+            <h2>Java</h2>
+            <h2>Javascript</h2>
+        </div>
+    )
+}
